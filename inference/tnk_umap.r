b8a38b07101c10753e94100b9058837e3fb3cc65
sanyuktaadap <sanyuadap@gmail.com>
2023-07-19 20:52:38 -0400

    Adding code to generate visualizations

--- inference/tnk_umap.r
+++ inference/tnk_umap.r
@@ -38,6 +38,7 @@ crc_intersect_ncells <- ncol(crc_sc_intersect)
 lc_intersect_ncells <- ncol(lc_sc_intersect)
 total_intersect_ncells <- lc_intersect_ncells + crc_intersect_ncells
 
+######samples is creating a single column with #crc cells + #lc cells
 samples <- matrix(
     c(rep("crc", crc_intersect_ncells), rep("lc", lc_intersect_ncells)),
     total_intersect_ncells,
@@ -53,14 +54,43 @@ load("inference/lung_cancer_TNK.RData")
 crc_value_matrix <- nHDP_trained_mb$count_matrix
 lc_value_matrix <- inferred_data$count_matrix
 
-#####you need to set the gem value for breast cancer cells to be 0 (crc tnk gems)
+#####you need to set the gem value for lung cancer cancer cells to be 0 (crc tnk gems)
 n_dim <- dim(merged_sc_intersect)[2]
 
+# metadata requires rows as cells. So we are creating a matrix with n_dim rows
+# dim(add_crc_tnk_gem_matrix) = 337787, 85
 add_crc_tnk_gem_matrix <- matrix(0, n_dim, 85)
+# filling first 246560 rows, (246560, 85) with crc value matrix (85, 246560)
+# thus need to transpose
+# NOTE: lc cell rows remain 0
 add_crc_tnk_gem_matrix[
     1: dim(crc_value_matrix)[2],
     ] <- t(crc_value_matrix)
 colnames(add_crc_tnk_gem_matrix) <- paste("crc_tnk", seq(1, 85, 1), sep = "")
+
+
+######to do: set the maximum value to 200 and update the add_crc_tnk_gem_matrix
+###### so that lower expression values in the umap are also visible clearly
+add_crc_tnk_gem_matrix[add_crc_tnk_gem_matrix > 200] <- 200
+
+
+######to do: only keep the GEMs expressed in more than 5% of cells
+print(sprintf("add_crc_tnk_gem_matrix dimensions before filtering GEMs: %d", dim(add_crc_tnk_gem_matrix)))
+
+# add_crc_tnk_gem_matrix has crc + lc cells. if we calculate nrows of this,
+# we might calculate 5% of the total, and lose more gems than expected
+crc_ncells <- ncol(crc_value_matrix)
+threshold <- crc_ncells * 0.05
+col_to_rm <- c()
+for (col in colnames(add_crc_tnk_gem_matrix)){
+    # count of genes with non-zero values less than equal to t will be invalid
+    if (sum(add_crc_tnk_gem_matrix[, col] > 0) <= threshold) {
+        col_to_rm <- c(col_to_rm, col)
+    }
+}
+add_crc_tnk_gem_matrix <- add_crc_tnk_gem_matrix[, !colnames(add_crc_tnk_gem_matrix) %in% col_to_rm]
+print(sprintf("add_crc_tnk_gem_matrix dimensions after filtering GEMs: %d", dim(add_crc_tnk_gem_matrix)))
+
 
 #####you need to set the gem value for crc cancer cells to be 0 (breast tnk gems)
 add_lc_tnk_gem_matrix <- matrix(0, n_dim, 85)
@@ -69,15 +99,46 @@ add_lc_tnk_gem_matrix[
     ] <- t(lc_value_matrix)
 colnames(add_lc_tnk_gem_matrix) <- paste("lc_tnk", seq(1, 85, 1), sep = "")
 
+
+######to do: set the maximum value to 200 and update the add_breast_tnk_gem_matrix
+add_lc_tnk_gem_matrix[add_lc_tnk_gem_matrix > 200] <- 200
+
+
+######to do: only keep the GEMs expressed in more than 5% of cells 
+######(save this for later. It seems that the new code leads to several 
+######GEMs with relatively small values)
+print(sprintf("add_lc_tnk_gem_matrix dimensions before filtering GEMs: %d", dim(add_lc_tnk_gem_matrix)))
+lc_ncells <- ncol(lc_value_matrix)
+threshold <- lc_ncells * 0.05
+col_to_rm <- c()
+for (col in colnames(add_lc_tnk_gem_matrix)){
+    # count of genes with non-zero values less than equal to t will be invalid
+    if (sum(add_lc_tnk_gem_matrix[, col] > 0) <= threshold) {
+        col_to_rm <- c(col_to_rm, col)
+    }
+}
+add_lc_tnk_gem_matrix <- add_lc_tnk_gem_matrix[, !colnames(add_lc_tnk_gem_matrix) %in% col_to_rm]
+print(sprintf("add_lc_tnk_gem_matrix dimensions after filtering GEMs: %d", dim(add_lc_tnk_gem_matrix)))
+
+
 #####this is where you incorporate the gem expression value into the seurat object
 metadata <- cbind(samples, add_crc_tnk_gem_matrix, add_lc_tnk_gem_matrix)
 
+# Saving col names
+crc_tnk_gem_names <- colnames(add_crc_tnk_gem_matrix)
+lc_tnk_gem_names <- colnames(add_lc_tnk_gem_matrix)
+
+rm(add_crc_tnk_gem_matrix, crc_tnk_input, add_lc_tnk_gem_matrix, crc_sc_intersect, CRC_TNK_Seuratobj, crc_value_matrix, lc_sc_intersect, lc_tnk_input, lc_value_matrix, query_df_sparse, X)
+gc()
+
 # Creating a seurat object with merged_crc_lc
 merged_crc_lc <- CreateSeuratObject(
+    # counts has genes * cells
     counts = merged_sc_intersect,
     project = "crc_lc_sc_dataset",
     min.cells = 3,
     min.features = 200,
+    # cells * features
     meta.data = metadata)
 
 # Normalizing the data
@@ -113,38 +174,77 @@ DimHeatmap(merged_crc_lc, dims = 1:15, cells = 500, balanced = TRUE)
 merged_crc_lc <- FindNeighbors(merged_crc_lc, dims = 1:10)
 merged_crc_lc <- FindClusters(merged_crc_lc, resolution = 0.5)
 
-# Look at cluster IDs of the first 5 cells
-# head(Idents(merged_crc_lc), 5)
 
 # Run non-linear dimension reduction
 merged_crc_lc <- RunUMAP(merged_crc_lc, dims = 1:30, reduction = "harmony")
 
-###### Combined UPMAP is ready! ######
+
+# Plot 1: Merged DimPlot
+jpeg("inference/results/merged_dimplot.jpeg",
+  res = 300,
+  width = 2000,
+  height = 2000)
+print(DimPlot(merged_crc_lc, reduction = "harmony", raster = FALSE))
+dev.off()
 
 
-#####Plot UMAP using for loop (all the crc gems)
-for (i in 1:dim(crc_value_matrix)[2]){
+# Plot 2: FP samples (mergerd crc & lc UMAP)
+jpeg("inference/results/FeaturePlots/samples.jpeg",
+  res = 300,
+  width = 2000,
+  height = 2000)
+print(DimPlot(merged_crc_lc, group.by = "samples", raster = FALSE))
+dev.off()
+
+
+# Plot 3: Feature Plots for particular T-cell marker genes
+features <- list("CD8A", "CD4", "MKI67", "CXCL13", "PDCD1", "CTLA4", "CCR7", "CXCR3", "CXCR5", "ICOS")
+for (feat in features){
   jpeg(
-    paste("inference/crc_tnk_umap/", "crc_tnk_cell_feature_gem_", i, ".jpeg", sep = ""),
+    paste("inference/results/FeaturePlots/", feat, ".jpeg", sep = ""),
     res = 300,
     width = 2000,
     height = 2000)
   print(FeaturePlot(
-    object = merged_crc_lc, features = paste("crc_tnk", i, sep = ""),
+  object = merged_crc_lc,
+  features = feat,
+  cols = c("grey85", "red"), 
+  raster = FALSE))
+dev.off()
+}
+
+
+# Plot 4: UMAP for selected GEMs
+#####Plot UMAP using for loop (all the crc gems)
+for (gem_name in colnames(add_crc_tnk_gem_matrix)){
+  jpeg(
+    paste("inference/results/crc_tnk_umap/", gem_name, ".jpeg", sep = ""),
+    res = 300,
+    width = 2000,
+    height = 2000)
+  print(FeaturePlot(
+    object = merged_crc_lc, features = gem_name,
     cols = c("grey85", "red"), raster = FALSE))
   dev.off()
 }
 
 #####example of all GEMs plot using for loop (all the lc gems)
-for (i in 1:dim(lc_value_matrix)[2]){
+for (gem_name in colnames(add_lc_tnk_gem_matrix)){
   jpeg(
-    paste("inference/lc_tnk_umap/", "lc_tnk_cell_feature_gem_", i, ".jpeg", sep = ""),
+    paste("inference/results/lc_tnk_umap/", gem_name, ".jpeg", sep = ""),
     res = 300,
     width = 2000,
     height = 2000)
   print(FeaturePlot(
     object = merged_crc_lc,
-    features = paste("lc_tnk", i, sep = ""),
+    features = gem_name,
     cols = c("grey85", "red"), raster = FALSE))
   dev.off()
 }
+
+
+# Plot 5: Wordcloud of top genes associated with a GEM
+
+# Plot 6: Enriched Signaliing Pathway With a GEM
+
+# Plot 7: Pick some GEMs to compare the difference between crc and breast
